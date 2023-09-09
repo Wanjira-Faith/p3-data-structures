75eb39d44862097c791eccfc06b9486be064487d
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-09 16:04:39 +0300

    added solution to sequences

---
+++ sequences.py
@@ -0,0 +1,27 @@
+# Write a function remove_duplicates(sequence) that takes a 
+# sequence (list or tuple) and returns a new sequence with duplicates 
+# removed while maintaining the original order of elements. 
+
+# sample input = 
+
+# input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+# result = remove_duplicates(input_sequence)
+# print(result)  # Output: [2, 3, 4, 5, 6, 7]
+
+
+# Solution
+def remove_duplicates(sequence):
+    seen = set()
+    result = []
+    
+    for item in sequence:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    
+    return result
+
+
+input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+result = remove_duplicates(input_sequence)
+print(result)  
